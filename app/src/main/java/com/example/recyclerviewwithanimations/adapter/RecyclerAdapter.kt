f767b79b51555ba76017cad4b93ee8e39c215359
Asif Jawaid <asif.jawaid@celeritas-solutions.com>
2020-02-28 12:24:31 +0500

    Complete Example

--- app/src/main/java/com/example/recyclerviewwithanimations/adapter/RecyclerAdapter.kt
+++ app/src/main/java/com/example/recyclerviewwithanimations/adapter/RecyclerAdapter.kt
@@ -5,34 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AnimationUtils
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.example.recyclerviewwithanimations.R
+import com.example.recyclerviewwithanimations.model.NewsModel
 import kotlinx.android.synthetic.main.animated_item.view.*
-import java.text.SimpleDateFormat
-import java.util.*
 import kotlin.collections.ArrayList
 
-class RecyclerAdapter(/*var summaryList: ArrayList<NewsModel>*/var context: Context) : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+class RecyclerAdapter(var context: Context, var newsList: ArrayList<NewsModel>) : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
 
-    private lateinit var circleImageView: ImageView
-    private lateinit var txt_title: TextView
-    private lateinit var txt_desc: TextView
-    private lateinit var txt_time: TextView
-    private lateinit var constraintLayout: ConstraintLayout
-
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        init {
-            circleImageView = itemView.findViewById(R.id.imageView)
-            txt_title = itemView.findViewById(R.id.txt_title)
-            txt_time = itemView.findViewById(R.id.txt_date)
-            txt_desc = itemView.findViewById(R.id.txt_desc)
-            txt_desc = itemView.findViewById(R.id.txt_desc)
-            constraintLayout = itemView.findViewById(R.id.constraintLayout)
-        }
-    }
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {}
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.animated_item, parent, false)
@@ -40,21 +21,15 @@ class RecyclerAdapter(/*var summaryList: ArrayList<NewsModel>*/var context: Cont
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-//        val item = summaryList[position]
+        val item = newsList[position]
         holder.itemView.imageView.animation = AnimationUtils.loadAnimation(context,R.anim.fade_transition_animation)
         holder.itemView.constraintLayout.animation = AnimationUtils.loadAnimation(context,R.anim.fade_scale_animation)
-//        txt_title.text = item.pet_name
-//        txt_score.text = item.survey_count.toString()
-//        txt_breed.text = item.breed
-//        txt_time.text = time
-
-//        Glide.with(context)
-//            .load(IMAGE_BASE_URL + "" + item.img_id)
-//            .into(circleImageView)
-
-
+        holder.itemView.txt_title.text = item.title
+        holder.itemView.txt_date.text = item.date
+        holder.itemView.txt_desc.text = item.description
+        holder.itemView.txt_title.text = item.title
+        holder.itemView.imageView.setImageResource(item.image)
     }
 
-    override fun getItemCount() = 22
-
+    override fun getItemCount() = newsList.size
 }
---
+++ app/src/main/java/com/example/recyclerviewwithanimations/model/NewsModel.kt
@@ -0,0 +1,3 @@
+package com.example.recyclerviewwithanimations.model
+
+data class NewsModel(var title: String = "", var description: String = "", var date: String = "", var image: Int = 0)
